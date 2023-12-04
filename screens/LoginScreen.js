f4e72049d25c7ef30e30cc6d1468839d53b7ee62
Vishal Singh <vishal9044singh@gmail.com>
2023-12-05 02:30:27 +0530

    changes for login and signup screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -27,21 +27,21 @@ export default function LoginScreen() {
         style={{borderTopLeftRadius: 50, borderTopRightRadius: 50}} 
         className="flex-1 bg-white px-8 pt-8">
           <View className="form space-y-2">
-            <Text className="text-gray-700 ml-4">Email Address</Text>
+            <Text className="text-gray-700 ml-4">Mobile Number</Text>
             <TextInput 
               className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-              placeholder="email"
-              value="john@gmail.com" 
+              placeholder="Enter Mobile Number..."
+              // value="john@gmail.com" 
             />
-            <Text className="text-gray-700 ml-4">Password</Text>
+            <Text className="text-gray-700 ml-4">MPIN</Text>
             <TextInput 
               className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
               secureTextEntry
-              placeholder="password"
-              value="test12345" 
+              placeholder="Enter MPIN..."
+              // value="test12345" 
             />
             <TouchableOpacity className="flex items-end">
-              <Text className="text-gray-700 mb-5">Forgot Password?</Text>
+              <Text className="text-gray-700 mb-5">Forgot MPIN?</Text>
             </TouchableOpacity>
             <TouchableOpacity 
               className="py-3 bg-yellow-400 rounded-xl">
@@ -53,7 +53,7 @@ export default function LoginScreen() {
              </TouchableOpacity>
             
           </View>
-          <Text className="text-xl text-gray-700 font-bold text-center py-5">Or</Text>
+          {/* <Text className="text-xl text-gray-700 font-bold text-center py-5">Or</Text>
           <View className="flex-row justify-center space-x-12">
             <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
               <Image source={require('../assets/icons/google.png')} className="w-10 h-10" />
@@ -64,7 +64,7 @@ export default function LoginScreen() {
             <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
               <Image source={require('../assets/icons/facebook.png')} className="w-10 h-10" />
             </TouchableOpacity>
-          </View>
+          </View> */}
           <View className="flex-row justify-center mt-7">
               <Text className="text-gray-500 font-semibold">
                   Don't have an account?
